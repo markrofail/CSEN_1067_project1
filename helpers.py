7b0577a4ee77ca095577e4cbc5afe0651abc149f
Mark Rofail <markm.rofail@gmail.com>
2018-10-26 18:14:07 +0200

    feat: finish Task1 and Task2

---
+++ helpers.py
@@ -0,0 +1,48 @@
+import itertools
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def results_parser(gs):
+    params = gs.cv_results_['params']
+    params = [', '.join("{!s}={!r}".format(key,val) for (key,val) in x.items()) for x in params]
+
+    scores = gs.cv_results_['mean_test_score']
+    rank = gs.cv_results_['rank_test_score']
+    return(np.array(sorted(zip(rank, scores, params))))  
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
